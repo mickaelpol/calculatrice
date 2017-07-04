f62c2164a5607da6165e21433c4345673ca8227d
Mickael <polmickael3@gmail.comgit config --global user.name>
2017-07-04 13:50:12 +0200

    ajout d'addition + multiplication

---
+++ calculatrice/calc.js
@@ -0,0 +1,7 @@
+var nombre1 = Number(prompt ("Saisie un chiffre?"));
+var nombre2 = Number(prompt ("Saisie en un autre?"));
+alert ("la somme des nombres est " + (nombre1+nombre2));
+alert ("la multiplication des nombres est " + (nombre1*nombre2));
+
+
+
